3608e6abe047763dd3c68553a69e5ed15f59144f
Vartul Chanana <vartulchanana@gmail.com>
2025-06-30 17:19:27 +0530

    Update 2025-06-30.py

--- LeetCode/2025-06-30.py
+++ LeetCode/2025-06-30.py
@@ -1,1 +1,14 @@
+from collections import Counter
+from typing import List
 
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        frequency = Counter(nums)
+        max_length = 0
+        
+        for num in frequency:
+            if num + 1 in frequency:
+                current_length = frequency[num] + frequency[num + 1]
+                max_length = max(max_length, current_length)
+        
+        return max_length
