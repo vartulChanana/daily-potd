abcee6e7325f603d29b8b7d35aca1b2ce56a0374
Vartul Chanana <vartulchanana@gmail.com>
2025-07-03 21:09:50 +0530

    Create 2025-07-03.py

---
+++ LeetCode/2025-07-03.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        def next_char(c):
+            return 'a' if c == 'z' else chr(ord(c) + 1)
+
+        word = "a"
+        while len(word) < k:
+            new_part = ''.join(next_char(c) for c in word)
+            word += new_part
+        return word[k - 1]
