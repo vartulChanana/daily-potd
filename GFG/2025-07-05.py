e66a72b99f1145a756886fbb8f052e742adb0251
Vartul Chanana <vartulchanana@gmail.com>
2025-07-05 03:40:23 +0530

    Update 2025-07-05.py

--- GFG/2025-07-05.py
+++ GFG/2025-07-05.py
@@ -1,1 +1,11 @@
+class Solution:
+    def maxSum(self, arr):
+        max_score = 0
+        n = len(arr)
 
+        for i in range(n - 1):
+            score = arr[i] + arr[i + 1]
+            if score > max_score:
+                max_score = score
+
+        return max_score
