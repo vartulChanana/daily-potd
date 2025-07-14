b9996941e66053c3204f99539358c0de62ef7dda
Vartul Chanana <vartulchanana@gmail.com>
2025-07-15 01:06:09 +0530

    Update 2025-07-14.py

--- LeetCode/2025-07-14.py
+++ LeetCode/2025-07-14.py
@@ -1,1 +1,16 @@
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+from typing import Optional
+
+class Solution:
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        num = 0
+        while head:
+            num = (num << 1) | head.val
+            head = head.next
+        return num
