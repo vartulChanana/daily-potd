6010d9bc3c7ff21d9c6341a66a7ee0b2a96d8d3c
Vartul Chanana <vartulchanana@gmail.com>
2025-07-21 23:11:20 +0530

    Update 2025-07-21.py

--- LeetCode/2025-07-21.py
+++ LeetCode/2025-07-21.py
@@ -1,52 +1,15 @@
-from typing import List, Dict
-from collections import defaultdict
-
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.name = ""
-        self.del_flag = False
-
 class Solution:
-    def deleteDuplicateFolder(self, paths: List[List[str]]) -> List[List[str]]:
-        root = TrieNode()
-        for path in paths:
-            node = root
-            for folder in path:
-                if folder not in node.children:
-                    node.children[folder] = TrieNode()
-                    node.children[folder].name = folder
-                node = node.children[folder]
+    def makeFancyString(self, s: str) -> str:
+        result = [s[0]]
+        count = 1
 
-        serial_map = defaultdict(list)
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                count += 1
+            else:
+                count = 1
 
-        def serialize(node):
-            if not node.children:
-                return ""
-            serial = []
-            for child_name in sorted(node.children.keys()):
-                child = node.children[child_name]
-                sub_serial = serialize(child)
-                serial.append(f"{child_name}({sub_serial})")
-            serial_str = "".join(serial)
-            serial_map[serial_str].append(node)
-            return serial_str
+            if count < 3:
+                result.append(s[i])
 
-        serialize(root)
-
-        for nodes in serial_map.values():
-            if len(nodes) > 1:
-                for node in nodes:
-                    node.del_flag = True
-
-        result = []
-
-        def dfs(node, path):
-            for name, child in node.children.items():
-                if child.del_flag:
-                    continue
-                result.append(path + [name])
-                dfs(child, path + [name])
-
-        dfs(root, [])
-        return result
+        return ''.join(result)
