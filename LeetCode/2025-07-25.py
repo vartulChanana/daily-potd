2f568c1c6e1be05c1b8bcbcfe1aad1929ffbabcd
Vartul Chanana <vartulchanana@gmail.com>
2025-07-25 22:08:35 +0530

    Update 2025-07-25.py

--- LeetCode/2025-07-25.py
+++ LeetCode/2025-07-25.py
@@ -1,1 +1,11 @@
+from typing import List
 
+class Solution:
+    def maxSum(self, nums: List[int]) -> int:
+        unique_nums = set(nums)
+        positive_sum = sum(num for num in unique_nums if num > 0)
+        
+        if positive_sum == 0:
+            return max(nums)
+        
+        return positive_sum
