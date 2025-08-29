28ac5b29c2cd63b8ca2adbe29bb461ee589a3f81
Vartul Chanana <vartulchanana@gmail.com>
2025-08-29 08:38:17 +0530

    Create 20250-08-29.py

---
+++ LeetCode/20250-08-29.py
@@ -0,0 +1,8 @@
+class Solution:
+    def flowerGame(self, n: int, m: int) -> int:
+        odd_n = (n + 1) // 2
+        even_n = n // 2
+        odd_m = (m + 1) // 2
+        even_m = m // 2
+        
+        return odd_n * even_m + even_n * odd_m
