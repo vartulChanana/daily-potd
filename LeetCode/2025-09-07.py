069860e1f4ade360991c2049e4e0cfbf12e10e9b
Vartul Chanana <vartulchanana@gmail.com>
2025-09-07 23:09:46 +0530

    Create 2025-09-07.py

---
+++ LeetCode/2025-09-07.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        res = []
+        for i in range(1, n // 2 + 1):
+            res.append(-i)
+            res.append(i)
+        if n % 2 == 1:
+            res.append(0)
+        return res
